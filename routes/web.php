14c3e8011b6ba3073ae6ae0148fcf1e2037c819e
= <=>
2022-02-11 10:20:34 +0700

    Add route /addprofile

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/addprofile', 'WebController@addprofile')->name('web.addprofile');
