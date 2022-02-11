00808b0999028e18fc8a2a7552e803eebce4bed4
= <=>
2022-02-11 10:23:48 +0700

    create function addprofile()

---
+++ app/Http/Controllers/WebController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WebController extends Controller
+{
+    Public function addprofile(){
+        return view("addprofile");
+    }
+}
