0f6a62556dd083ff7f18559ba72170fba4c39baf
= <=>
2022-02-11 11:18:58 +0700

    Edit form show name and surname

---
+++ resources/views/addprofile.blade.php
@@ -0,0 +1,6 @@
+<div class="form-group">
+    <label for="usr">Name:</label>
+    <input type="text" class="form-control" id="usr" value="{{$name}}">
+    <label for="sur">SureName:</label>
+    <input type="text" class="form-control" id="sur" value="{{$surname}}">
+</div>
