91c3d4121e05c7b50765848788cdc837c48e5857
= <=>
2022-02-18 08:52:35 +0700

    Use argon layouts

--- resources/views/addprofile.blade.php
+++ resources/views/addprofile.blade.php
@@ -1,6 +1,11 @@
+@extends('layouts.argon')
+@section('content')
+
 <div class="form-group">
     <label for="usr">Name:</label>
     <input type="text" class="form-control" id="usr" value="{{$name}}">
     <label for="sur">SureName:</label>
     <input type="text" class="form-control" id="sur" value="{{$surname}}">
 </div>
+
+@endsection
