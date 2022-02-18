66f84db27b5c8870bf73e7692cf04db80f96ca3c
= <=>
2022-02-18 09:20:21 +0700

    Add route /register

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/home', 'HomeController@index')->name('home');
-// Route::get('/addprofile', 'WebController@addprofile')->name('web.addprofile');
-// Route::get('/addprofile', 'App\Http\Controllers\WebController@addprofile');
+
 Route::get('/addprofile', [WebController::class, 'addprofile']);
 
+Route::get('/register', 'App\Http\Controllers\RegistrationController@create');
+Route::post('register', 'App\Http\Controllers\RegistrationController@store');
