bceab517b77572c2b8a072f7b0d555fbcaa8db9c
= <=>
2022-02-18 11:44:45 +0700

    Create route view product_type and product page

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,6 @@
 use App\Http\Controllers\WebController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -26,3 +15,6 @@ Route::get('/addprofile', [WebController::class, 'addprofile']);
 
 Route::get('/register', 'App\Http\Controllers\RegistrationController@create');
 Route::post('register', 'App\Http\Controllers\RegistrationController@store');
+
+Route::resource('product_types', 'App\Http\Controllers\ProductTypeController');
+Route::resource('products', 'App\Http\Controllers\ProductController');
