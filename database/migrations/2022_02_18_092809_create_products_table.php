6295f32ed4181211ccae5a08d7f957d1646c7ad2
= <=>
2022-02-18 09:40:00 +0700

    Setting column table product_types and producs

--- database/migrations/2022_02_18_092809_create_products_table.php
+++ database/migrations/2022_02_18_092809_create_products_table.php
@@ -14,8 +14,15 @@ return new class extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->decimal('cost',8,2);
+            $table->decimal('price',8,2);
+            $table->decimal('quantity');
+            $table->string('image')->nullable();
+            $table->bigInteger('product_type_id')->unsigned();
             $table->timestamps();
+            $table->foreign('product_type_id')->references('id')->on('product_types');
         });
     }
 
